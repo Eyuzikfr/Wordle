bc36311a6e8408dc766707fcca96901f00670bb5
Eyuzikfr <eyuzikchhunzu2@gmail.com>
2025-04-16 12:23:52 +0545

    add word display feature

--- src/js/components/Main.jsx
+++ src/js/components/Main.jsx
@@ -1,26 +1,95 @@
 import "/src/css/Main.css";
 
 export default function Main() {
-  const array = ["R", "O", "G", "E", "R"];
+  const word = "REACT";
+  const guessedWords = [
+    [
+      {
+        letter: "R",
+        isInWord: true,
+        isInPosition: true,
+      },
+      {
+        letter: "O",
+        isInWord: false,
+        isInPosition: false,
+      },
+      {
+        letter: "G",
+        isInWord: false,
+        isInPosition: false,
+      },
+      {
+        letter: "E",
+        isInWord: true,
+        isInPosition: false,
+      },
+      {
+        letter: "R",
+        isInWord: false,
+        isInPosition: false,
+      },
+    ],
+  ];
+
   const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   alphabets.push("Enter", "Delete");
 
-  const arrayElements = array.map((letter, index) => {
-    return (
-      <div className="letter-tile" key={index}>
-        {letter}
-      </div>
-    );
-  });
+  const arrayElements = guessedWords.map((word, index) =>
+    word.map((element, index) => {
+      return (
+        <div
+          key={index}
+          className="letter-tile"
+          style={{
+            backgroundColor: element.isInWord
+              ? element.isInPosition
+                ? "#60AF33"
+                : "#D4C32A"
+              : "#E2E2E2",
+          }}
+        >
+          {element.letter}
+        </div>
+      );
+    })
+  );
+
+  const buttonClick = (event) => {
+    console.log(event.target.value);
+  };
 
   const keyboardElements = alphabets.map((letter, index) => {
     if (index === 26) {
-      return <div className="keyboard-tile enter-key">{letter}</div>;
+      return (
+        <button
+          className="keyboard-tile enter-key"
+          key={index}
+          onClick={buttonClick}
+          value={letter}
+        >
+          {letter}
+        </button>
+      );
     } else if (index === 27) {
-      return <div className="keyboard-tile delete-key">{letter}</div>;
+      return (
+        <button
+          className="keyboard-tile delete-key"
+          key={index}
+          onClick={buttonClick}
+          value={letter}
+        >
+          {letter}
+        </button>
+      );
     } else {
       return (
-        <button className="keyboard-tile" key={index}>
+        <button
+          className="keyboard-tile"
+          key={index}
+          onClick={buttonClick}
+          value={letter}
+        >
           {letter}
         </button>
       );
